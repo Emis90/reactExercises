254e7776d64bbaabaf32783db1f3c597f77ba92b
Fayan1103 <enida.vezelaj@hotmail.it>
2019-12-27 12:32:08 -0500

    fetched data from api in my componentdidmount in pagination component

--- showcase/src/Pagination.js
+++ showcase/src/Pagination.js
@@ -1,12 +1,27 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 
-const Pagination = () => {
-   return(
-     <div>
-       <h1>This is the pagination page</h1>
-     </div>
-   )
+class Pagination extends React.Component {
+  constructor(){
+    super()
+    this.state = {}
+  }
+
+  async componentDidMount(){
+    let result = await axios.get("https://pokeapi.co/api/v2/pokemon")
+    console.log(result.data)
+    this.setState(result.data)
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>This is the pagination page</h1>
+      </div>
+    )
+  }
+
 }
 
 
