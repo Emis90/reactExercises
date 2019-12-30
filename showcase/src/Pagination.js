3535e773362598b9768f3a98b62851a55588f79b
Fayan1103 <enida.vezelaj@hotmail.it>
2019-12-30 15:05:33 -0500

    learning how to use React hooks: useState

--- showcase/src/Pagination.js
+++ showcase/src/Pagination.js
@@ -1,26 +1,57 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, {useState} from 'react'
+// import axios from 'axios'
 
+const Pagination = () => {
+const [ pages, changePages ] = useState(1)
+const [name, changeName] = useState({first: '', last: ''})
+const [note, setNote] = useState({word: '', list: []})
 
-class Pagination extends React.Component {
-  constructor(){
-    super()
-    this.state = {}
-  }
-
-  async componentDidMount(){
-    let result = await axios.get("https://pokeapi.co/api/v2/pokemon")
-    console.log(result.data)
-    this.setState(result.data)
-  }
-
-  render() {
+const add = (e) => {
+  e.preventDefaul()
+  setNote({
+    ...note,
+    list: [...note.list, e.target.value]
+  })
+}
     return(
       <div>
-        <h1>This is the pagination page</h1>
+      <div className='counter'>
+        <h1>Change pages</h1>
+        <button onClick={()=> changePages(prev => prev + 1)}>next</button>
+        <h2>{pages}</h2>
+        <button onClick={()=> changePages(prev => prev > 0 ? prev - 1 : prev = 0)}>previous</button>
+        </div>
+
+      <div className='name'>
+        <form>
+          <input type="text" value={name.first}
+           onChange={(e)=> changeName({...name, first: e.target.value})}
+          />
+          <input type="text" value={name.last}
+            onChange={(e)=> changeName({...name, last: e.target.value})}
+          />
+          <h3>Your name is: {name.first} {name.last}</h3>
+        </form>
+
+      </div>
+
+      <div className="notes">
+        <form onSubmit={add}>
+          <input type="text" placeholder="add note here" value={note.word}
+            onChange={(e) => setNote({...note, word: e.target.value})}
+          />
+          <button>Add</button>
+          <ul>
+          {
+            note.list.map((word, i) => (<li key={i}>{word}</li>))
+          }
+        </ul>
+        </form>
+
+      </div>
       </div>
     )
-  }
+
 
 }
 
