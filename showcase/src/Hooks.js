a4367b5cce18a474cd212820f2ec60a83e79555a
Fayan1103 <enida.vezelaj@hotmail.it>
2020-01-02 14:13:50 -0500

    Pokemon pagination, useEffect to fetch data with axios

---
+++ showcase/src/Hooks.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+
+function Hooks() {
+    const [ pages, changePages ] = useState(1)
+    const [name, changeName] = useState({first: '', last: ''})
+    const [note, setNote] = useState({word: '', list: []})
+
+    const add = (e) => {
+      e.preventDefault();
+      setNote({word:'', list: [...note.list, note.word]})
+    }
+
+        return(
+          <div>
+                <div className="notes">
+            <form onSubmit={add}>
+              <input type="text" placeholder="add note here" value={note.word}
+                onChange={(e) => setNote({...note, word: e.target.value})}
+              />
+              <button>Add</button>
+              <ul>
+              {
+                note.list.map((word, i) => (<li key={i}>{word}</li>))
+              }
+            </ul>
+            </form>
+
+          </div>
+          <div className='counter'>
+            <p>Change pages</p>
+            <button onClick={()=> changePages(prev => prev + 1)}>next</button>
+            <h2>{pages}</h2>
+            <button onClick={()=> changePages(prev => prev > 0 ? prev - 1 : prev = 0)}>previous</button>
+            </div>
+
+          <div className='name'>
+            <form>
+              <input type="text" value={name.first}
+               onChange={(e)=> changeName({...name, first: e.target.value})}
+              />
+              <input type="text" value={name.last}
+                onChange={(e)=> changeName({...name, last: e.target.value})}
+              />
+              <h3>Your name is: {name.first} {name.last}</h3>
+            </form>
+
+          </div>
+          </div>
+        )
+
+
+    }
+
+export default Hooks
--- showcase/src/Pagination.js
+++ showcase/src/Pagination.js
@@ -1,58 +1,47 @@
-import React, {useState} from 'react'
-// import axios from 'axios'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 
 const Pagination = () => {
-const [ pages, changePages ] = useState(1)
-const [name, changeName] = useState({first: '', last: ''})
-const [note, setNote] = useState({word: '', list: []})
 
-const add = (e) => {
-  e.preventDefaul()
-  setNote({
-    ...note,
-    list: [...note.list, e.target.value]
-  })
+const [ state, updateState ] = useState({pokemons: [], data: {}, next: '', prev: ''})
+
+const fetchData = async(URL) => {
+  let {data} = await axios.get(URL)
+  console.log('data >>', data)
+  updateState({pokemons: data.results, next: data.next, prev: data.previous})
+  }
+
+useEffect(()=> {
+  let URL = 'https://pokeapi.co/api/v2/pokemon';
+  fetchData(URL)
+
+},[URL])
+
+const nextPage = () => {
+  if (state.next === null) {
+    return
+  }
+ URL = state.next
 }
-    return(
-      <div>
-      <div className='counter'>
-        <h1>Change pages</h1>
-        <button onClick={()=> changePages(prev => prev + 1)}>next</button>
-        <h2>{pages}</h2>
-        <button onClick={()=> changePages(prev => prev > 0 ? prev - 1 : prev = 0)}>previous</button>
-        </div>
 
-      <div className='name'>
-        <form>
-          <input type="text" value={name.first}
-           onChange={(e)=> changeName({...name, first: e.target.value})}
-          />
-          <input type="text" value={name.last}
-            onChange={(e)=> changeName({...name, last: e.target.value})}
-          />
-          <h3>Your name is: {name.first} {name.last}</h3>
-        </form>
-
-      </div>
-
-      <div className="notes">
-        <form onSubmit={add}>
-          <input type="text" placeholder="add note here" value={note.word}
-            onChange={(e) => setNote({...note, word: e.target.value})}
-          />
-          <button>Add</button>
-          <ul>
-          {
-            note.list.map((word, i) => (<li key={i}>{word}</li>))
-          }
-        </ul>
-        </form>
-
-      </div>
-      </div>
-    )
-
-
+const prevPage = () => {
+  if (state.previous === null) {
+    return
+  }
+ URL = state.previous
+}
+   return (
+     <div>
+      {
+        state.pokemons.map((poke, i) => {
+        return <p key={i}>{poke.name} <br/><img src={poke.ur}/></p>
+        })
+      }
+      <button onClick={prevPage}>Prev</button>
+      <button onClick={nextPage}>Next</button>
+     </div>
+   )
 }
 
 
