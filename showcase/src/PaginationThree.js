f3224abbf14e4620c50c106d504f04f5975466c8
Fayan1103 <enida.vezelaj@hotmail.it>
2020-01-03 14:56:21 -0500

    pagination II works, started pagination III

--- showcase/src/PaginationThree.js
+++ showcase/src/PaginationThree.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
 function PaginationThree() {
+  const [pokes, setPokes]  = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [pokesPerPage, setPokesPerPage] = useState(3)
+
+  useEffect(()=> {
+
+    const getData = async() =>{
+      setLoading(true);
+      const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon');
+      setPokes(data.results)
+      setLoading(false)
+    }
+
+    getData()
+  }, [])
+
+  console.log(pokes)
+
   return (
     <div>
+    {loading ? (<p>loading</p>) : (<p>Poke page</p>)}
 
     </div>
   )
--- showcase/src/PaginationTwo.js
+++ showcase/src/PaginationTwo.js
@@ -3,47 +3,61 @@ import axios from 'axios'
 
 function PaginationTwo() {
 const [book, updateBook] = useState({content: {}})
+const [currentPage, updatePage] = useState(book.content['page1'])
 
 useEffect(() => {
   const getData = async() => {
   let {data} = await axios.get('https://pokeapi.co/api/v2/pokemon')
   // console.log('data >>', data)
-  let pokePerPage = 7
+  let pokePerPage = 2
   let pokemons = data.results
   let k = 0;
   let page = 1
   let allPages = {}
 
   while (k < pokemons.length) {
-    let subArr = pokemons.slice(k, k + pokePerPage)
+    let subArr = pokemons.slice(k, pokePerPage + k)
     let group = []
     for (let i = 0; i < subArr.length; i++){
     let poke = subArr[i]
     group.push(poke)
-    allPages[`page${page}`] = [...group, poke]
+    allPages[`page${page}`] = [...group]
   }
   page ++
   k = k + pokePerPage
 }
-console.log(allPages)
+
+let firstPage = allPages[`page1`]
+
 updateBook({content: allPages})
+updatePage(firstPage)
 }
 
- getData()
+getData()
 
-}, [])
 
-console.log(book.content)
+},[])
+
+
+const changePage = (idx) => {
+   updatePage(book.content[`page${idx}`])
+}
+
+// console.log(currentPage)//its undefined!
   return (
     <div>
-
+      {
+       book.content && Object.keys(book.content).map((pages, idx)=> {
+         return <button key={idx} onClick={()=>changePage(idx + 1)}>{idx + 1}</button>
+       })
+     }
      {
-       book.content['page2'] && book.content['page2'].map((poke, id) => {
-
+       currentPage && currentPage.map((poke, id) => {
          return <p key={id}>{poke.name}</p>
 
        })
      }
+
     </div>
   )
 }
